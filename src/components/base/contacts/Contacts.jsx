78ef1ffcf8b7d3856e1c5d5a96d48eca750ae75c
Andii Karamushko <karamushko1997@gmail.com>
2026-02-09 23:39:19 +0200

    ref: add dropdown to telephone

--- src/components/base/contacts/Contacts.jsx
+++ src/components/base/contacts/Contacts.jsx
@@ -7,7 +7,8 @@ import {
   DropdownMenu,
   Button,
 } from "@heroui/react";
-import Link from "next/link";
+// import Link from "next/link";
+import { Link } from "@/i18n/routing";
 import { BsTelephone } from "react-icons/bs";
 import { CiMail } from "react-icons/ci";
 import SocialList from "@/data/social";
@@ -15,13 +16,44 @@ import SocialList from "@/data/social";
 const Contacts = ({ data }) => {
   return (
     <ul className="flex items-center gap-4 flex-col text-white lg:flex-row lg:gap-2">
-      <li className="bg-primary p-4 rounded-full">
-        <Link href="tel:+380979863778" className="flex gap-2 items-center">
+      <li className="">
+        {/* <Link href="tel:+380979863778" className="flex gap-2 items-center">
           <i>
             <BsTelephone />
           </i>
           <span className="hidden lg:block">0979863778</span>
-        </Link>
+        </Link> */}
+        <Dropdown shouldBlockScroll={false}>
+          <DropdownTrigger>
+            <Button
+              className="bg-primary p-4 h-auto min-w-0 text-white"
+              radius="full"
+            >
+              <i>
+                <BsTelephone />
+              </i>
+              <span className="hidden lg:block">Позвонить мне</span>
+            </Button>
+          </DropdownTrigger>
+          <DropdownMenu>
+            <DropdownItem>
+              <Link
+                href={"tel:+30979863778"}
+                className="flex items-center gap-4"
+              >
+                +30979863778
+              </Link>
+            </DropdownItem>
+            <DropdownItem>
+              <Link
+                href={"tel:+380982892372"}
+                className="flex items-center gap-4"
+              >
+                +380982892372
+              </Link>
+            </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
       </li>
       <li>
         <Dropdown shouldBlockScroll={false}>
