870f0be4c2f4900b2a00a849579cbf8ae6dbfe4d
Andii Karamushko <karamushko1997@gmail.com>
2026-02-13 22:33:45 +0200

    ref: changed styles for h1 on main page

--- src/components/page-home/hero-section/HeroSection.jsx
+++ src/components/page-home/hero-section/HeroSection.jsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import HeroButtonsGroup from "./HeroButtonsGroup";
+import Heading from "@/components/UI/heading/Heading";
 
 const HeroSection = ({ data }) => {
   return (
@@ -15,14 +16,14 @@ const HeroSection = ({ data }) => {
       <div className="absolute inset-0 bg-secondary/70 -z-10" />
       <div className="hero-section__content ml-6 mr-6 flex flex-col justify-evenly items-center h-dvh text-center text-white">
         <div className="hero-section__content--content flex flex-col gap-8 mb-10 px-8">
-          <h1 className="hero-section__title text-5xl custom-h1">
+          <Heading level="h1" align="center" className="text-4xl">
             Владислав Рясний
-          </h1>
+          </Heading>
 
-          <p className="hero-section__description text-2xl custom-h1">
+          <Heading level="h2" align="center">
             адвокат у сфері цивільного, військового, кримінального,
             адміністративного та сімейного права
-          </p>
+          </Heading>
         </div>
         <HeroButtonsGroup
           textModal={"Открыть модалку"}
